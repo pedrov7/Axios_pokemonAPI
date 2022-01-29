204f4286599642bad01331be13df8635a0f94aea
pedrov7 <pedro7valla@hotmail.com>
2022-01-29 12:55:58 -0500

    cambios para traer en el boton la API

--- src/components/DrawWithAxios.jsx
+++ src/components/DrawWithAxios.jsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import axios from 'axios';
 
 export const DrawWithAxios = () => {
 
-    const [list, setList] = useState(null);
-    const [drawPokemon, setDrawPokemon] = useState(false);
-
-    useEffect(
-        () => {
-            axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1200')
-                .then(response => { setList(response.data.results) })
-        }
-
-        , []);
+    const [list, setList] = useState([]);
+   
 
     const handleOnClick = () => {
-        // console.log(list);
-        setDrawPokemon(!drawPokemon);        
+        axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1200')
+                .then(response => { setList(response.data.results) })  
+                .catch( e => alert(e))
     }
 
 
@@ -29,12 +22,12 @@ export const DrawWithAxios = () => {
             <button onClick={handleOnClick} type="button" className="btn btn-warning">Click to Fetch Pokemon List</button>
             <br></br>
             <br></br>
-            {drawPokemon && <div className="list-group">
+            <div className="list-group">
                 {list.length > 0 && list.map((pokeName, index) => {
                     // return (<li key={index}>{pokeName.name}</li>)
                     return(<button key = {index} type="button" className="list-group-item list-group-item-action">{pokeName.name}</button>)
                 }
                 )}
-            </div>}
+            </div>
         </div>);
 };
